54214711574fc4033a3b6f8507cc872f3560fcbf
zain100000 <muhammadzainulabideen292@gmail.com>
2024-05-20 09:51:56 +0500

    added new server

--- server/app.js
+++ server/app.js
@@ -30,9 +30,7 @@ mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connected to MongoDB successfully!");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
-    });
+    app.listen();
   })
   .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
